29a6c3049c6d5d17e58ad8d3da4404456e309b44
rezarekt <theveyst12@gmail.com>
2021-04-10 23:23:51 +0800

    requirements changed

--- src/app/link.service.ts
+++ src/app/link.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class LinkService {
 
   apiLink: string = "https://cc106.rxmsolutions.com/";
-  consumer_key: string  = "ck_xxxx";
-  consumer_secret: string  = "cs_xxxx";
+  consumer_key: string  = "ck_a0474739d22e7300fdc8f283f2426847ef2a621c";
+  consumer_secret: string  = "cs_0061f0c07aa91435148ed294877e1e46cd70454a";
   constructor() { }
 
   getAPILink() : string{
